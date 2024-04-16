8c3c34966b33183d4cc3cbd0ca856f347751e5f9
Jennifer Simond <jenniasimond@gmail.com>
2024-04-16 01:22:02 -0400

    update project

---
+++ react-router-proj/src/App.jsx
@@ -0,0 +1,20 @@
+
+import MainSection from './components/MainSection'
+import Navbar from './components/Navbar'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div id="container">
+      <h1 className='app-h1'>Hello React Router!</h1>
+      <Navbar/>
+      <MainSection />
+    </div> 
+    </>
+  )
+}
+
+export default App
